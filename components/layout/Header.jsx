868b27353fa1f6bf55fab7b6fd854c2526cbd063
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-26 19:28:18 -0300

    Finalizando header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,36 +1,36 @@
+import styles from './Header.module.scss';
+
 export default function Header(){
     return (
-        <>
-        <div>
+        <div className={styles.header}>
             <div>
-                <img src="" alt="" />
-            </div>
-            <ul>
-                <li><a href="">Roupas</a></li>
-                <li><a href="">Calçados</a></li>
-                <li><a href="">Acessórios</a></li>
-                <li><a href="">Ofertas</a></li>
-                <li><a href="">Sobre Nós</a></li>
-            </ul>
-            <div>
+                <a href="#">Sua Loja</a>
                 <ul>
-                    <li><a href="">Login</a></li>
-                    <li><a href="">Carrinho</a></li>
+                    <li><a href="">Roupas</a></li>
+                    <li><a href="">Calçados</a></li>
+                    <li><a href="">Acessórios</a></li>
+                    <li><a href="">Ofertas</a></li>
+                    <li><a href="">Sobre Nós</a></li>
                 </ul>
+                <div>
+                    <ul>
+                        <li><a href="">Login</a></li>
+                        <li><a href="">Carrinho</a></li>
+                    </ul>
+                </div>
             </div>
+            <nav>
+                <ul>
+                    <li><a href="">Peças Superiores</a></li>
+                    <li><a href="">Peças Inferiores</a></li>
+                    <li><a href="">Calçados</a></li>
+                    <li><a href="">Acessórios</a></li>
+                </ul>
+                <div>
+                    <input type="text" name="" id="" />
+                    <button type="submit">Pesquisar</button>
+                </div>
+            </nav>
         </div>
-        <nav>
-            <ul>
-                <li><a href="">Peças Superiores</a></li>
-                <li><a href="">Peças Inferiores</a></li>
-                <li><a href="">Calçados</a></li>
-                <li><a href="">Acessórios</a></li>
-            </ul>
-            <div>
-                <input type="text" name="" id="" />
-                <button type="submit">Pesquisar</button>
-            </div>
-        </nav>
-        </>
     );
 }
